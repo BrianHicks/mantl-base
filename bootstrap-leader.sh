f92e4e40553452c1c2b687442d486dc43e775d1f
Brian Hicks <brian@brianthicks.com>
2016-01-27 11:48:26 -0600

    leaders: enable nomad client

--- bootstrap-leader.sh
+++ bootstrap-leader.sh
@@ -33,9 +33,15 @@ jq --argjson ips $IPS \
    --arg id $(cat /etc/mantl/id) \
    --arg private_ip $(cat /etc/mantl/private_ip) \
    --arg public_ip $(cat /etc/mantl/public_ip) \
+   --arg role $(cat /etc/mantl/role) \
    '.bind_addr = $private_ip
     | .server.bootstrap_expect = ($ips | length)
-    | .server.retry_join = $ips' \
+    | .server.retry_join = $ips
+    | .client.servers = [$ips | .[] | "\(.):4647"]
+    | .client.node_id = $id
+    | .client.node_class = $role
+    | .client.meta.public_ip = $public_ip
+    | .client.meta.private_ip = $private_ip' \
    > /etc/nomad/nomad.json \
    <<EOF
 {
@@ -45,7 +51,10 @@ jq --argjson ips $IPS \
         "enabled": true
     },
     "client": {
-        "enabled": false
+        "enabled": true,
+        "options": {
+            "driver.whitelist": "docker,exec,raw_exec"
+        }
     }
 }
 EOF
